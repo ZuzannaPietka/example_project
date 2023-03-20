0beb73c180e1cb71aafbd789e0aa8591dc7d48f0
Zuza <zuzannapietka00@gmail.com>
2023-03-20 12:21:13 +0100

    Added first task

---
+++ exercise/ex_1.py
@@ -0,0 +1,13 @@
+def hello(a: str) -> str:
+    # TODO
+    return "Hello"
+
+
+print("Example")
+print(hello("Ola"))
+
+assert hello("Ola") == "Hello Ola"
+assert hello("Zuzia") == "Hello Zuzia"
+
+print("Zadanie zrobione")
+
