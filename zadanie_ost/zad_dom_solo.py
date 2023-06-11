7d24621fe2ea0934425d8fefc02cefc5680b37a9
Zuzanna Piętka <74881@student.ue.poznan.pl>
2023-06-11 13:56:23 +0200

    zad 1

---
+++ zadanie_ost/zad_dom_solo.py
@@ -0,0 +1,14 @@
+# Funkcja suma_listy(l) zwraca sumę wszystkich elementów w liście l.
+# Jeśli lista jest pusta, zwraca 0.
+# W przeciwnym przypadku, dodaje pierwszy element listy do sumy reszty listy.
+
+def suma_listy(l):
+    if not l:  # Sprawdzenie, czy lista jest pusta
+        return 0
+    else:
+        return l[0] + suma_listy(l[1:])
+
+# Przykład użycia:
+lista = [3, 9, 8, 1, 2]
+wynik = suma_listy(lista)
+print(wynik)  # Wyświetli 23
